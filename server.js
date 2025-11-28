01a86594b7cecd7b96234b0777add8d8dafe0713
wolvsrcool <wolfscrool25@gmail.com>
2025-11-28 16:03:32 +0200

    rollback

--- server.js
+++ server.js
@@ -1,23 +1,10 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-
-// Explicit middleware array
-const middlewares = jsonServer.defaults({
-  bodyParser: true,
-  logger: true,
-  readOnly: false,
-});
-
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001;
 
 server.use(middlewares);
-
-server.use((req, res, next) => {
-  console.log("Query params:", req.query);
-  next();
-});
-
 server.use(router);
 
 server.listen(port, () => {
