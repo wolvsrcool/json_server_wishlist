b05566d11ac9bbe5467c7c9c9c6ec501abbcf662
wolvsrcool <wolfscrool25@gmail.com>
2025-11-28 16:01:07 +0200

    test-fix

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+
+// Explicit middleware array
+const middlewares = jsonServer.defaults({
+  bodyParser: true,
+  logger: true,
+  readOnly: false,
+});
+
 const port = process.env.PORT || 3001;
 
 server.use(middlewares);
+
+server.use((req, res, next) => {
+  console.log("Query params:", req.query);
+  next();
+});
+
 server.use(router);
 
 server.listen(port, () => {
